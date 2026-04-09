5489867ffbd85b3458dd5b7a223e28a95f834851
Yurii-Leniv <supusman32@gmail.com>
2026-04-09 11:14:12 +0300

    ailehrqugf

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,24 @@
 'use strict';
 
 const firstPromise = new Promise((resolve, reject) => {
-  document.addEventListener('click', () => {
-    resolve('First promise was resolved');
+  const timerId = setTimeout( () => {
+    reject(new Error('First promise was rejected'));
+  }, 3000);
+
+  document.addEventListener('click', (e) => {
+    if (e.button === 0) {
+      clearTimeout(timerId);
+      resolve('First promise was resolved');
+    }
   })
 
-  setTimeout( () => {
-    reject('First promise was rejected');
-  }, 3000);
 });
 
 const secondPromise = new Promise((resolve, reject) => {
-  document.addEventListener('mousedown', () => {
-    resolve('Second promise was resolved');
+  document.addEventListener('mousedown', (e) => {
+    if (e.button === 0 || e.button === 2) {
+      resolve('Second promise was resolved');
+    }
   })
 });
 
@@ -41,11 +47,11 @@ function successCase(message) {
   document.body.append(div);
 }
 
-function failCase(message) {
+function failCase(error) {
   const div = document.createElement('div');
   div.setAttribute('data-qa', 'notification');
   div.classList.add('error');
-  div.textContent = message;
+  div.textContent = error.message;
   document.body.append(div);
 }
 
